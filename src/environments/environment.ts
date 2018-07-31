55cda4fba51862add3ea7893f0f24394c78c7e79
jagmeet <jagmeet.alagh@gmail.com>
2018-07-31 20:28:37 +0530

    initialized angular application with firestore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false
+  firebase: {
+  	apiKey: "AIzaSyD27jahYCwaXSj-fwf29x1sIZ-QvXho6o8",
+    authDomain: "clientpanel-9f92d.firebaseapp.com",
+    databaseURL: "https://clientpanel-9f92d.firebaseio.com",
+    projectId: "clientpanel-9f92d",
+    storageBucket: "",
+    messagingSenderId: "642306270976"
+  }
 };
-
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
